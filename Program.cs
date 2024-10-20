d64e0c315df8f93db0e718effe45641ac264a508
Romstar744 <romstar742@gmail.com>
2024-10-20 12:30:48 +0300

    Fixed snowflake, add comments and finish project

--- Program.cs
+++ Program.cs
@@ -1,18 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace FNA_Snowfall_Starostin
 {
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        /// Основная точка входа в приложение.
         /// </summary>
         static void Main(string[] args)
         {
-            using (Snow game = new Snow())
+            using (var game = new Snow())
             {
                 game.Run();
             }
